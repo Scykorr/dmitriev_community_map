3e6f3a5a57077bc29edd1b2ee1377ac80d64ce93
Tony <fedosov-toxa@mail.ru>
2024-04-14 18:03:20 +0300

    add support forms

--- main_new_own.py
+++ main_new_own.py
@@ -4,15 +4,15 @@
 import sys
 from PyQt5 import QtWidgets
 from GUIpy import main_new_own, set_scale
+from GUIpy.get_result import Ui_FormGetResult
+from GUIpy.result_table import Ui_Form_muft_list
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMenu, QAction, qApp
 
 from modules import imgView
 
-
-
-
-
+first_window_muft_params = []
+params_muft_after_click = []
 
 class MyMainWindow(QtWidgets.QMainWindow, main_new_own.Ui_MainWindow):
     def __init__(self):
@@ -24,6 +24,7 @@ class MyMainWindow(QtWidgets.QMainWindow, main_new_own.Ui_MainWindow):
         self.createActions(self.imageView)
         self.createMenus()
         self.setWindowTitle('Построение оптического линейного тракта')
+        self.get_result_window = WindowGetResult()
 
     def fitToWindow(self):
         fitToWindow = self.fitToWindowAct.isChecked()
@@ -50,11 +51,11 @@ class MyMainWindow(QtWidgets.QMainWindow, main_new_own.Ui_MainWindow):
         self.aboutQtAct = QAction("О &Qt", self, triggered=qApp.aboutQt)
         self.figures = QAction("&Фигуры", self, triggered=view.paintEvent)
         self.add_flag = QAction("&Добавить точку", self, triggered=view.paintEvent)
-        self.get_result = QAction("&Произвести рассчет", self, triggered=view.paintEvent)
+        self.get_result = QAction("&Произвести рассчет", self, triggered=self.getResult)
         self.scaling = QAction("&Масштабирование", self, enabled=False, triggered=view.scaling_img)
 
-
-
+    def getResult(self):
+        self.get_result_window.show()
 
     def createMenus(self):
         self.fileMenu = QMenu("&Файл", self)
@@ -80,7 +81,6 @@ class MyMainWindow(QtWidgets.QMainWindow, main_new_own.Ui_MainWindow):
         self.toolMenu.addAction(self.scaling)
         self.toolMenu.addAction(self.get_result)
 
-
         self.helpMenu = QMenu("&Помощь", self)
         self.helpMenu.addAction(self.aboutAct)
         self.helpMenu.addAction(self.aboutQtAct)
@@ -90,6 +90,37 @@ class MyMainWindow(QtWidgets.QMainWindow, main_new_own.Ui_MainWindow):
         self.menuBar().addMenu(self.toolMenu)
         self.menuBar().addMenu(self.helpMenu)
 
+
+class WindowGetResult(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui_get_result = Ui_FormGetResult()
+        self.ui_get_result.setupUi(self)
+        self.get_hint()
+        self.get_list_form = MuftList()
+        self.ui_get_result.comboBox_5.currentTextChanged.connect(self.get_hint)
+        self.ui_get_result.pushButton_get_muft_list.clicked.connect(self.show_muft_list)
+
+    def get_hint(self):
+        if self.ui_get_result.comboBox_5.currentText() == 'МТОК-А1/216-1KT3645-K-77':
+            self.ui_get_result.comboBox_5.setToolTip('Усиленная муфта, отличающаяся высочайшей надежностью.')
+        elif self.ui_get_result.comboBox_5.currentText() == 'МТОК-Б1/216-1KT3645-K-44':
+            self.ui_get_result.comboBox_5.setToolTip('Муфта с транзитной петлёй.')
+        elif self.ui_get_result.comboBox_5.currentText() == 'МТОК-В2/216-1KT3645-K-44':
+            self.ui_get_result.comboBox_5.setToolTip('Муфта с транзитной петлёй. Механическая герметизация корпуса.')
+        elif self.ui_get_result.comboBox_5.currentText() == 'МТОК-М6/144-1KT3645-K-44':
+            self.ui_get_result.comboBox_5.setToolTip(
+                'Бюджетный вариант – минимальная цена. Малогабаритная муфта. Механическая герметизация корпуса.')
+
+    def show_muft_list(self):
+        self.get_list_form.show()
+
+
+class MuftList(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        QtWidgets.QWidget.__init__(self, parent)
+        self.ui_get_list = Ui_Form_muft_list()
+        self.ui_get_list.setupUi(self)
 
 def main():
     app = QApplication(sys.argv)
